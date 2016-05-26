007281914a1c1a32fe1310ed7b29200f70800fbf
张俊 <zhangjun166@pingan.com.cn>
2016-05-26 16:59:17 +0800

    update MainActivity  add scroll control

--- app/src/main/java/com/lovejjfg/blogdemo/activity/MainActivity.java
+++ app/src/main/java/com/lovejjfg/blogdemo/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.lovejjfg.blogdemo.activity;
 
+import android.animation.Animator;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 import com.lovejjfg.blogdemo.R;
 import com.lovejjfg.blogdemo.ui.AnimUtils;
 import com.lovejjfg.blogdemo.utils.BaseUtil;
+import com.lovejjfg.blogdemo.utils.JumpUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -40,6 +42,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     int minOffset = 0;
     private int offset;
     private Interpolator interpolator;
+    private int btHeight;
+    private boolean isShown = false;
+    private int currentDy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,16 +57,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         scrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
             // TODO: 2016/5/25 滑动相关的逻辑
+
             @Override
             public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                int dy = scrollY - oldScrollY;
                 Log.e(TAG, "onScrollChange: " + scrollY);
-                setOffset(mTv2, scrollY);
+                if (dy > 50) {
+                    buttonOutAnimation(interpolator);
+                } else if (dy < -50) {
+                    buttonEnterAnimation(interpolator);
+                } else {
+                    setBtOffset(mBt, dy, btHeight);
+                }
+                setOffset(mTv2, scrollY, minOffset);
             }
         });
 
-//        if (stick != null) {
-//            stick.setTranslationY(100);
-//        }
+        scrollView.setOnTouchListener(new View.OnTouchListener() {
+
+            private boolean flag;
+            private int startY;
+
+            @Override
+            public boolean onTouch(View v, MotionEvent ev) {
+                switch (ev.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        if (currentDy == 0 || currentDy == btHeight) {
+                            break;
+                        }
+                        if (currentDy >= btHeight * 0.4) {
+                            isShown = true;
+                            buttonOutAnimation(interpolator);
+                        } else {
+                            isShown = false;
+                            buttonEnterAnimation(interpolator);
+                        }
+                        break;
+
+                }
+                return false;
+            }
+        });
+
         mTvSlide.post(new Runnable() {
 
 
@@ -89,7 +126,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mBtSheet.setOnClickListener(this);
     }
 
-    public void setOffset(View view, int offset) {
+    private void setBtOffset(Button mBt, int dy, int btHeight) {
+        currentDy = (int) (mBt.getTranslationY() + dy);
+        if (currentDy < 0) {
+            currentDy = 0;
+            isShown = true;
+        } else if (currentDy > btHeight) {
+            currentDy = btHeight;
+            isShown = false;
+        }
+        if (mBt.getTranslationY() != currentDy) {
+            mBt.setTranslationY(currentDy);
+            ViewCompat.postInvalidateOnAnimation(mBt);
+        }
+
+
+    }
+
+    public void setOffset(View view, int offset, int minOffset) {
 
         offset = Math.max(minOffset, offset);
         if (offset > 105) {
@@ -100,39 +154,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             ViewCompat.postInvalidateOnAnimation(view);
         }
     }
+
     private void buttonEnterAnimation(Interpolator interp) {
-        int startY = mBt.getMeasuredHeight();
-        Log.i(TAG, "buttonEnterAnimation: Bottom:" + mBt.getBottom() + "::height:" + mBt.getMeasuredHeight());
-        mBt.setTranslationY(startY);
+        if (isShown) {
+            return;
+        }
+        btHeight = mBt.getMeasuredHeight();
+        mBt.setTranslationY(currentDy == 0 ? btHeight : currentDy);
         mBt.setAlpha(0.5f);
         mBt.animate()
                 .translationY(0)
                 .alpha(1f)
                 .setDuration(200)
                 .setInterpolator(interp)
-                .setListener(null)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        isShown = true;
+                        Log.i(TAG, "onEnterAnimationStart: " + isShown);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        currentDy = 0;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                })
                 .start();
     }
+
     private void buttonOutAnimation(Interpolator interp) {
-        int startY = mBt.getMeasuredHeight();
-        Log.i(TAG, "buttonEnterAnimation: Bottom:" + mBt.getBottom() + "::height:" + mBt.getMeasuredHeight());
-        mBt.setTranslationY(0);
-        mBt.setAlpha(0.5f);
+        if (!isShown) {
+            return;
+        }
+        mBt.setTranslationY(currentDy == 0 ? 0 : currentDy);
         mBt.animate()
-                .translationY(startY)
-                .alpha(1f)
+                .translationY(btHeight)
                 .setDuration(200)
                 .setInterpolator(interp)
-                .setListener(null)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        isShown = false;
+                        Log.i(TAG, "onOutAnimationStart: " + isShown);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        currentDy = btHeight;
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                })
                 .start();
     }
 
     private void viewEnterAnimation(View view, float offset, Interpolator interp) {
         view.setTranslationY(offset);
-        view.setAlpha(0.5f);
         view.animate()
                 .translationY(0f)
-                .alpha(1f)
                 .setDuration(300)
                 .setInterpolator(interp)
                 .setListener(null)
@@ -166,7 +263,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.bottom_sheet:
 
-                BaseUtil.startActivityOnspecifiedAnimation(this, BottomSheetActivity.class);
+//                BaseUtil.startActivityOnspecifiedAnimation(this, BottomSheetActivity.class);
+                JumpUtil.jumpToBottome(this);
                 break;
             case R.id.button:
                 buttonOutAnimation(interpolator);
