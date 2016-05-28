2868a6cfd1d58b778f2a5b3574691cfbd300c2b5
zhangjun123 <lovejjfg@163.com>
2016-05-28 15:29:08 +0800

    create Constants.java

---
+++ app/src/main/java/com/lovejjfg/blogdemo/base/Constants.java
@@ -0,0 +1,12 @@
+package com.lovejjfg.blogdemo.base;
+
+/**
+ * Created by Joe on 2016/5/28.
+ * Email lovejjfg@gmail.com
+ */
+public class Constants {
+    public static final String BASE_URL = "file:///android_asset/";
+    public static final String MIME_TYPE = "text/html";
+    public static final String ENCODING = "utf-8";
+    public static final String FAIL_URL = "http://blog.csdn.net/lovejjfg/";
+}
