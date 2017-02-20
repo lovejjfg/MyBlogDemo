5f2bf300efacbd4b96d86b7e638133434f09b2e2
zhangjun <zhangjun@ehousechina.com>
2017-02-20 13:39:24 +0800

    fit image to StatusBar

--- app/src/main/java/com/lovejjfg/blogdemo/activity/ScrollingActivity.java
+++ app/src/main/java/com/lovejjfg/blogdemo/activity/ScrollingActivity.java
@@ -1,7 +1,5 @@
 package com.lovejjfg.blogdemo.activity;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.v4.widget.NestedScrollView;
@@ -10,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,12 +27,17 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
     ImageView mIv;
     @Bind(R.id.tv1)
     TextView mTv1;
+    boolean showStatus = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling1);
         ButterKnife.bind(this);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+
         mIv.setOnClickListener(this);
 //        mView.setOnClickListener(this);
         AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
@@ -45,32 +49,33 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
 //                }
 //            });
 //        }
-        scrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
-            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-            @Override
-            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                Log.e("scrollY:", scrollY + "");
-                Log.e("mTv1", "translateY: " + mTv1.getTranslationY());
-                Log.e("mTv1", "scroolY: " + mTv1.getScrollY());
-                Log.e("mTv1", "Top: " + mTv1.getTop());
-                Log.e("mTv1", "Bottom: " + mTv1.getBottom());
-                int minimumHeight = mIv.getMinimumHeight();
-                int measuredHeight = mIv.getMeasuredHeight();
-                int dy = measuredHeight - minimumHeight;
-                int scrool = Math.min(dy, scrollY);
-                mIv.setTranslationY((float) (-scrool*0.8));
-                if (scrool == dy) {
-                    mIv.setPressed(true);
-                    mIv.bringToFront();
-                    mIv.requestLayout();
-                    mIv.getParent().requestLayout();
-//                    mIv.refreshDrawableState();
-                } else {
-                    mIv.setPressed(false);
-//                    mIv.refreshDrawableState();
-                }
-            }
-        });
+
+//        scrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+//            @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+//            @Override
+//            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+//                Log.e("scrollY:", scrollY + "");
+//                Log.e("mTv1", "translateY: " + mTv1.getTranslationY());
+//                Log.e("mTv1", "scroolY: " + mTv1.getScrollY());
+//                Log.e("mTv1", "Top: " + mTv1.getTop());
+//                Log.e("mTv1", "Bottom: " + mTv1.getBottom());
+//                int minimumHeight = mIv.getMinimumHeight();
+//                int measuredHeight = mIv.getMeasuredHeight();
+//                int dy = measuredHeight - minimumHeight;
+//                int scrool = Math.min(dy, scrollY);
+//                mIv.setTranslationY((float) (-scrool*0.8));
+//                if (scrool == dy) {
+//                    mIv.setPressed(true);
+//                    mIv.bringToFront();
+//                    mIv.requestLayout();
+//                    mIv.getParent().requestLayout();
+////                    mIv.refreshDrawableState();
+//                } else {
+//                    mIv.setPressed(false);
+////                    mIv.refreshDrawableState();
+//                }
+//            }
+//        });
         mTv1.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
             public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
@@ -82,12 +87,6 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
         });
 
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        findViewById(R.id.tv1).setOnClickListener(this);
-//        findViewById(R.id.tv2).setOnClickListener(this);
-//        findViewById(R.id.tv3).setOnClickListener(this);
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
 
     }
@@ -96,6 +95,8 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
     public void onClick(View v) {
         toast.setText(v.getId() + "");
         toast.show();
+        showStatus = !showStatus;
+        full(showStatus);
     }
 
     @Override
@@ -110,6 +111,20 @@ public class ScrollingActivity extends AppCompatActivity implements View.OnClick
         toast.setText(item.getItemId() + "");
         toast.show();
         return true;
+    }
+
+    private void full(boolean enable) {
+        if (enable) {
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.flags |= WindowManager.LayoutParams.FLAG_FULLSCREEN;
+            getWindow().setAttributes(lp);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        } else {
+            WindowManager.LayoutParams attr = getWindow().getAttributes();
+            attr.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().setAttributes(attr);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
     }
 
 }
